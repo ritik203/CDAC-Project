658e6371350bc637e7c9ff71ab6242cfcf51cc69
ritik203 <ritikwaghamare@gmail.com>
2025-02-15 11:06:36 +0530

    added show api

--- User_Backend/controller/movie.js
+++ User_Backend/controller/movie.js
@@ -25,7 +25,46 @@ connection.query(query,(error,result)=>{
 });
 
 
+app.get("/show",(req,res)=>{
+    const {movieName,theatreName,showDate}=req.body;
 
+    const query=`   SELECT 
+        t.name AS theatre_name,
+        m.movie_name,
+        s.show_date,
+        TIME_FORMAT(ti.start_time, '%H:%i') AS start_time,
+        TIME_FORMAT(ti.end_time, '%H:%i') AS end_time
+    FROM 
+        theatre t
+    JOIN 
+        movie_theatre mt ON t.theatre_id = mt.theatre_id
+    JOIN 
+        movie m ON mt.movie_id = m.movie_id
+    JOIN 
+        \`show\` s ON mt.movie_id = s.movie_id AND mt.theatre_id = t.theatre_id
+    JOIN 
+        time ti ON s.time_id = ti.time_id
+    WHERE 
+        m.movie_name = ?
+        AND t.name = ?
+        AND s.show_date = ?
+    `;
+
+   connection.query(query,[movieName,theatreName,showDate],(error,result)=>{
+    res.setHeader("content-type","application/json");
+    if(error){
+        console.log(error);
+        res.status(500).json({status:"error",data:"could not get data"});
+    }else{
+        res.json({status:"success",data:result});
+    }
+   });
+
+
+
+
+
+});
 
 
 
