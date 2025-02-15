b8b5744661c6dd075c994476d5e0f7b18bc7a445
ritik203 <ritikwaghamare@gmail.com>
2025-02-15 10:46:25 +0530

    added theatre  controller

---
+++ User_Backend/controller/theatre.js
@@ -0,0 +1,23 @@
+const express =require("express");
+const connection=require("../connection");
+
+const app=express.Router();
+
+app.get("/city/:city_id",(req,res)=>{
+
+    const {city_id}=req.params;
+    const query="select * from theatre where city_id=?";
+
+    connection.query(query,[city_id],(err,result)=>{
+        res.setHeader("content-type","application/json");
+        if(err){
+         console.log(err);
+         res.status(500).json({status:"error"});
+
+        }  else{
+            res.json({status:"success",data:result});
+        }
+      })
+});
+
+module.exports=app;
